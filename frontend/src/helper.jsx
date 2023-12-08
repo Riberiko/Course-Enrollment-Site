2f655f98358aa27105397aa690809059267c5093
Riberiko <albericriko@gmail.com>
2023-12-08 00:56:48 -0800

    too much of a headache, but user validation done on frontend

--- frontend/src/helper.jsx
+++ frontend/src/helper.jsx
@@ -1,25 +1,23 @@
 import {useUser} from './userContext'
 
-function statusCheck(response, errorMessage = 'from backend')
+async function statusCheck(response, errorMessage = 'from backend')
 {
-    console.log(response)
     const contentType = response.headers.get('Content-Type');
-    const message = (contentType.includes('text')) ? response.text() : response.json()
+    const message = (contentType.includes('text')) ? await response.text() : await response.json()
     if(!response.ok) throw Error('[Error] : '+ message)
     return response
 }
 
-function isAuthF(userData)
+async function isAuthF()
 {
     return new Promise((resolve, reject)=>{
         fetch('http://localhost:8000/isAuth', {
             method: 'POST',
-            headers: {'Content-Type':'application/json'},
             credentials: 'include',
         })
         .then(res => statusCheck(res))
         .then(() => resolve(true))
-        .catch((err) => reject(err))
+        .catch(() => reject(false))
     })
 }
 
