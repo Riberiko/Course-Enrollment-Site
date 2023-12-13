dfe4e25d636b8e6527f80dfd4c94203115250730
Rob Tai <61960571+robtai29@users.noreply.github.com>
2023-12-12 22:35:24 -0800

    Update AccountInfo.jsx

--- frontend/src/components/AccountInfo.jsx
+++ frontend/src/components/AccountInfo.jsx
@@ -1,3 +1,9 @@
+/*
+  Name: Rob Tai, Michael Lezon, Riberiko Niyomwungere, David Ortega
+  Date: 12.12.23
+  This is react js file for Account Info page
+*/
+
 import { useEffect, useState } from "react"
 import { statusCheck } from "../helper"
 import ClassItem from "./ClassItem"
